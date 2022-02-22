724cafde5e6290404ecc63e76a2ce398ad8cce37
unknown <romanyefimets@gmail.com>
2022-02-22 09:29:19 -0800

    added category meny to filter menu by

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import { useState } from 'react';
 import menu from './data';
 import Categories from './categories.component';
 import Menu from './menu.component';
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+    const filtered = menu.filter((item) => item.category === category);
+    setMenuItems(filtered);
+  };
 
   return (
     <main>
@@ -15,8 +26,8 @@ const App = () => {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
-        <Menu items={menuItems}/>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
--- src/categories.component.jsx
+++ src/categories.component.jsx
@@ -1,5 +1,20 @@
-const Categories = () => {
-  return <div>Categories</div>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, key) => {
+        return (
+          <button
+            type="button"
+            key={key}
+            className="filter-btn"
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
